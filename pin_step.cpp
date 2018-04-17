df136613fd8e5d56736d331926f5a2955b7e7f75
Emil Harder <mail@emilharder.dk>
2018-04-17 21:42:37 +0000

    First throw at a new stepper-class. Next step (hehe) is a seperate thread. That way, it will behave more like the function gpioPWM in dc. Makes it possible to change speed (stopping) while the motor is running.

--- pin_step.cpp
+++ pin_step.cpp
@@ -1,94 +1,124 @@
 /*
- *  Program for testing c++ on the Pi and how to control the GPIOs with it.
- *  The library for using GPIOs is pigpio (http://abyz.me.uk/rpi/pigpio).
+ *  Class for running a stepper motor (kind of) as a normal dc motor.
  *
- *  To build the code, use the command:
- *
- *    g++ -o "name_of_output_file" "name_of_input_file".cpp -lpigpio -lrt
- *
- *  lpigpio includes the pigpio library and lrt is to include the library for realtime functionality
- *  (required by pigpio).
+ *  For use with 2semPi at https://github.com/Morgenkaff/2semPI
  */
 
-/*
- *  Misc vars as for user interaction:
- */
+#include <pigpio.h> // Library used to connect to the gpios
+#include "pin_base.h" // Base control of the gpios (led, switches) and definings for the pins
+#include "pin_step.h" // Header file for this class
 
-bool good_input = false;
-char user_input_char;
-int user_input_int;
+Pin_step::pin_step();{
+    _speed = 0;
+    _direction = 0;
+    _runStepper(_speed, _direction);
+}
 
-/*
- * Misc vars for controlling the program.
- */
+Pin_step::pin_step(bool direction, int speed);{
+    if (0 < speed < 256){
+        _direction = direction;
+        _speed = speed;
+    } else {
+        _speed = 0;
+        _direction = 0;
+    }
+    
+    _runStepper(_speed, _direction);
+}
 
-bool run_program = true; // True when the program-loop should be running
-bool run_motor = false; // True when the motor should be turning
-bool direction; // Sets wich direction to turn the motor
-int step_case; // Sets wich case in the switch to set the pins to
-int kill_switch_control; // Input for a potential kill switch
-bool kill_switch = false; // Is true when kill switch have been pressed
 
-/*
- * This function sets the GPIO used for the stepper motor.
- * This in a loop with the functions stepTraverse and loopCase, will it run
- * the stepper motor continuosly.
- */
-void stepper(){
+
+void Pin_dc::setSpeed(int speed){
+    if (0 < speed < 256){
+        _speed = speed;
+    } else {
+        // Do nothing
+    }
+    
+    _runStepper(_speed, _direction);
+}
+
+int Pin_dc::getSpeed(){
+    return _speed;
+}
+
+void Pin_dc::setDirection(bool direction){
+    _direction = direction;
+    
+    _runStepper(_speed, _direction);
+}
+
+bool Pin_dc::getDirection(){
+    return _direction;
+}
+
+void _runStepper(){
+    
+    int delay_time = 5000 - (_speed * (4300/255));
+    _step_case = 1;
+    
+    while (_speed != 0) {
+        _set_step(_step_case);
+        _stepTraverse();
+        _loopCase();
+        gpioDelay(delay_time);
+    }
+
+void _set_step(int& step_case){
         switch(step_case){
             case 0:
-                gpioWrite(BLU, LOW); 
-                gpioWrite(GRN, LOW);
-                gpioWrite(ORG, LOW);
-                gpioWrite(WHT, HIGH);
+                gpioWrite(STEP_1, LOW); 
+                gpioWrite(STEP_2, LOW);
+                gpioWrite(STEP_3, LOW);
+                gpioWrite(STEP_4, HIGH);
                 break; 
             case 1:
-                gpioWrite(BLU, LOW); 
-                gpioWrite(GRN, LOW);
-                gpioWrite(ORG, HIGH);
-                gpioWrite(WHT, HIGH);
+                gpioWrite(STEP_1, LOW); 
+                gpioWrite(STEP_2, LOW);
+                gpioWrite(STEP_3, HIGH);
+                gpioWrite(STEP_4, HIGH);
                 break; 
             case 2:
-                gpioWrite(BLU, LOW); 
-                gpioWrite(GRN, LOW);
-                gpioWrite(ORG, HIGH);
-                gpioWrite(WHT, LOW);
+                gpioWrite(STEP_1, LOW); 
+                gpioWrite(STEP_2, LOW);
+                gpioWrite(STEP_3, HIGH);
+                gpioWrite(STEP_4, LOW);
                 break; 
             case 3:
-                gpioWrite(BLU, LOW); 
-                gpioWrite(GRN, HIGH);
-                gpioWrite(ORG, HIGH);
-                gpioWrite(WHT, LOW);
+                gpioWrite(STEP_1, LOW); 
+                gpioWrite(STEP_2, HIGH);
+                gpioWrite(STEP_3, HIGH);
+                gpioWrite(STEP_4, LOW);
                 break; 
             case 4:
-                gpioWrite(BLU, LOW); 
-                gpioWrite(GRN, HIGH);
-                gpioWrite(ORG, LOW);
-                gpioWrite(WHT, LOW);
+                gpioWrite(STEP_1, LOW); 
+                gpioWrite(STEP_2, HIGH);
+                gpioWrite(STEP_3, LOW);
+                gpioWrite(STEP_4, LOW);
                 break; 
             case 5:
-                gpioWrite(BLU, HIGH); 
-                gpioWrite(GRN, HIGH);
-                gpioWrite(ORG, LOW);
-                gpioWrite(WHT, LOW);
+                gpioWrite(STEP_1, HIGH); 
+                gpioWrite(STEP_2, HIGH);
+                gpioWrite(STEP_3, LOW);
+                gpioWrite(STEP_4, LOW);
                 break; 
             case 6:
-                gpioWrite(BLU, HIGH); 
-                gpioWrite(GRN, LOW);
-                gpioWrite(ORG, LOW);
-                gpioWrite(WHT, LOW);
+                gpioWrite(STEP_1, HIGH); 
+                gpioWrite(STEP_2, LOW);
+                gpioWrite(STEP_3, LOW);
+                gpioWrite(STEP_4, LOW);
                 break; 
             case 7:
-                gpioWrite(BLU, HIGH); 
-                gpioWrite(GRN, LOW);
-                gpioWrite(ORG, LOW);
-                gpioWrite(WHT, HIGH);
+                gpioWrite(STEP_1, HIGH); 
+                gpioWrite(STEP_2, LOW);
+                gpioWrite(STEP_3, LOW);
+                gpioWrite(STEP_4, HIGH);
                 break; 
             default:
-                gpioWrite(BLU, LOW); 
-                gpioWrite(GRN, LOW);
-                gpioWrite(ORG, LOW);
-                gpioWrite(WHT, LOW);
+                gpioWrite(STEP_1, LOW); 
+                gpioWrite(STEP_2, LOW);
+                gpioWrite(STEP_3, LOW);
+                gpioWrite(STEP_4, LOW);
                 break; 
         }
 }
@@ -96,32 +126,17 @@ void stepper(){
 /*
  * This function traverse the step_case, depending on the direction.
  */
-void stepTraverse(){
-    if(direction==1){ step_case++;}
-    if(direction==0){ step_case--;}
+void _stepTraverse(){
+    if(_direction==1){ _step_case++;}
+    if(_direction==0){ _step_case--;}
 }
 
 /*
  * This function changes the var step_case, securing that the case switch
  * will "loop around".
  */
-void loopCase(){    
-    if(step_case>7){step_case=0;}
-    if(step_case<0){step_case=7;}
-}
-
-/*
- * base function for running the stepper motor.
- * It takes a bool parameter for the direction, 1 for clockwise 0 for counter.
- */
-void run_step_motor(bool p_direction){
-    
-    // Sets the direction as given
-    direction = p_direction;
-    run_motor = true;
-    
-    while (run_motor) {
-        stepper();
-        
+void _loopCase(){    
+    if(_step_case>7){_step_case=0;}
+    if(_step_case<0){_step_case=7;}
 }
 
