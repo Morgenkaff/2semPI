97bfaa7eb1a6c74827647045ba3d17e71b885fb8
Emil Harder <mail@emilharder.dk>
2018-05-11 14:55:45 +0000

    Cleaned up. Removed old fodlers etc.

--- tests/dc.cpp
+++
@@ -1,7 +0,0 @@
-#include "dc.h"
-#include <iostream>
-
-void Dc::hello(){
-    
-    std::cout << "Hello from DC" << std::endl;
-}
--- tests/stepper.cpp
+++
@@ -1,7 +0,0 @@
-#include "stepper.h"
-#include <iostream>
-
-void Stepper::hello(){
-    
-    std::cout << "Hello from Steper" << std::endl;
-}
