2f7e0642d8c93cea47a518af6c396383867ba13b
Morgenkaff <mail@emilharder.dk>
2018-05-20 21:16:13 +0200

    More log, and timer

--- src/timer.h
+++ src/timer.h
@@ -12,17 +12,20 @@
 #include <iostream>
 
 class Logger {
+private:
+    
+    //Vars used for timer
+    long int total_runtime_; // Stores total run time for motor, in seconds, since program start
+    long int runtime_start_time_; // Used to store seconds since epoch at motor-start
+    long int runtime_stop_time_; // Used to store seconds since epoch at motor-stop
+        
+    
 public:
     
     Timer();
     
     void start(); // Used to start the timer
     long int stop(); // Used to stop the timer, and return the duration
-    
-    //Vars used for timer
-    long int total_runtime_; // Stores total run time for motor, in seconds, since program start
-    long int runtime_start_time_; // Used to store seconds since epoch at motor-start
-    long int runtime_stop_time_; // Used to store seconds since epoch at motor-stop
     
 private:
     
